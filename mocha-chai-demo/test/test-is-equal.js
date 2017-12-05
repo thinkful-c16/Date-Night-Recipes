453549c249d8d3beede21707082f2b1f43d06a61
Bronson <keliiakimoto@gmail.com>
2017-12-04 19:21:21 -0800

    added dev dependencies and deployed heroku

---
+++ mocha-chai-demo/test/test-is-equal.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// import isEqual.js
+const isEqual = require('../isEqual');
+
+require('chai').should();
+
+// "describe" is used to declare the entity to be tested, and a callback function that sets up the tests
+describe('isEqual', function() {
+  // indicate the behavior to be tested
+  // typically the "it" statement will also include a callback that provides the test
+  it('should give right answers for equal and unequal inputs', function() {
+    // we're going to the `isEqual` function on a range of inputs
+    // that the function should return true for.
+    const equalInputs = [
+      [1, 1],
+      [true, true],
+      ['foo', 'foo']
+    ];
+    // loop through inputs and check that each set returns true
+    equalInputs.forEach(function(input) {
+      const answer = isEqual(input[0], input[1]);
+      // use `chai.should`'s keywords to set expectations for
+      // `isEqual`'s behavior
+      answer.should.be.true;
+    });
+
+    // range of inputs that the function should return false for
+    const unequalInputs = [
+      ['1', 1],
+      [1, 2],
+      [1, true],
+      [0, false]
+    ];
+    // loop through inputs and check that each set returns false
+    unequalInputs.forEach(function(input) {
+      const answer = isEqual(input[0], input[1]);
+      answer.should.be.false;
+    });
+  });
+}
+);
