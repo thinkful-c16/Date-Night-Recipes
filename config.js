0fb242b224e04672040a2f702ae1eb55659a98c0
Bronson <keliiakimoto@gmail.com>
2017-12-07 17:55:14 -0800

    uncommented db

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ exports.DATABASE_URL =
 'mongodb://datenight:datenight@ds133746.mlab.com:33746/date-night';
 
 exports.TEST_DATABASE_URL =
-// process.env.TEST_DATABASE_URL ||
-// global.TEST_DATABASE_URL ||
+process.env.TEST_DATABASE_URL ||
+global.TEST_DATABASE_URL ||
 'mongodb://datenight:datenight@ds133746.mlab.com:33746/date-night';
 
 exports.PORT = process.env.PORT || 8080;
